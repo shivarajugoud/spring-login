4b75bfcace31574d9377518fb75ca4bc0d256e7e
shivarajugoud <shivarajum127@gmail.com>
2021-03-28 14:50:53 +0530

    first commit

---
+++ src/main/java/io/javabrains/springlogin/User.java
@@ -0,0 +1,65 @@
+package io.javabrains.springlogin;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="User")
+public class User {
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String userName;
+    private String password;
+    private String roles;
+    private boolean active;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+
+
+
+}
---
+++ src/test/java/io/javabrains/springlogin/SpringLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.springlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
